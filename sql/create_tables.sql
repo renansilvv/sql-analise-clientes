7e0995d901ad7f64294a1b3c2d6a396235a95630
renansilvv <renanmsilvapo@gmail.com>
2026-03-31 08:12:12 -0300

    Create create_tables.sql

---
+++ sql/create_tables.sql
@@ -0,0 +1,10 @@
+CREATE TABLE sales (
+    order_date DATE,
+    product VARCHAR(150),
+    category VARCHAR(100),
+    region VARCHAR(100),
+    customer_name VARCHAR(150),
+    quantity INT,
+    sales DECIMAL(10,2),
+    profit DECIMAL(10,2)
+);

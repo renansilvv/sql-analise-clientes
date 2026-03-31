7ddab4c616b8fcdadb46db733077705f8c0c60f8
renansilvv <renanmsilvapo@gmail.com>
2026-03-31 09:13:17 -0300

    Update analysis.sql

--- sql/analysis.sql
+++ sql/analysis.sql
@@ -4,8 +4,10 @@ FROM sales;
 SELECT SUM(sales) AS receita_total
 FROM sales;
 
-SELECT SUM(profit) AS lucro_total
-FROM sales;
+SELECT region, SUM(sales) AS receita
+FROM sales
+GROUP BY region
+ORDER BY receita DESC;
 
 SELECT category, SUM(sales) AS receita
 FROM sales
@@ -18,26 +20,14 @@ GROUP BY product_name
 ORDER BY receita DESC
 LIMIT 10;
 
-SELECT region, SUM(sales) AS receita
-FROM sales
-GROUP BY region
-ORDER BY receita DESC;
-
 SELECT customer_name, SUM(sales) AS total_gasto
 FROM sales
 GROUP BY customer_name
 ORDER BY total_gasto DESC
 LIMIT 10;
 
-SELECT DATE_FORMAT(order_date, '%Y-%m') AS mes, SUM(sales) AS receita
+SELECT DATE_FORMAT(order_date, '%Y-%m') AS mes,
+       SUM(sales) AS receita
 FROM sales
-GROUP BY DATE_FORMAT(order_date, '%Y-%m')
+GROUP BY mes
 ORDER BY mes;
-
-SELECT category,
-       SUM(profit) AS lucro_total,
-       SUM(sales) AS receita_total,
-       ROUND((SUM(profit) / NULLIF(SUM(sales), 0)) * 100, 2) AS margem_percentual
-FROM sales
-GROUP BY category
-ORDER BY lucro_total DESC;
